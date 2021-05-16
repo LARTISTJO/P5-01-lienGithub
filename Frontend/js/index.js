e2c6926b0cc900d1a0d23b8ae37c6d57f43299ac
Johan <irejo@hotmail.fr>
2021-05-17 01:24:22 +0200

    fetch index

--- Frontend/js/index.js
+++ Frontend/js/index.js
@@ -1,15 +1,59 @@
 
-//...........................................................
+//........ API requète
+function appareilPhoto () {
+	fetch("http://localhost:3000/api/cameras")
+	.then( function (res) {
+		return res.json();
+	})
+	//........Structure html
+	.then ((data) => {
+		const structure = document.getElementById(liste-appareils-photo) ;
+		for (let i = 0; i < data.length; i++){ 
 
-let appareilPhoto;
+	//........Figure
+	const figure = document.createElement ("figure");
+	listeAppareilsPhoto.appendChild(figure);
+		
+		//Création de l'img au sein de l'élément figure
+		let img = document.createElement("img");
+		img.src= data [i].imageUrl;
+		img.alt = "appareil photo vintage";
+		figure.appendChild(img);
 
-// API REQUEST
-const fetchAppareilPhoto = async() => {
-	appareilPhoto = await fetch(
-		'http://localhost:3000/api/cameras').then(res => res.json());
+		//Création du figcaption au sein de l'élément figure
+		let figcaption= document.createElement("figcaption");
+		figure.appendChild(figcaption);
 
-        console.log(appareilPhoto);
-};
 
-fetchAppareilPhoto ();
+		//Mise en place du texte au sein de la figcaption
+			let description = document.createElement("div");
+			let prix = document.createElement("div");
+			description.setAttribute("class", "description");
+			figcaption.appendChild(description);
 
+			//Mise en place du texte au sein de la class description
+				let nameAppareil = document.createElement('h3');
+				let texte = document.createElement('p');
+				nameAppareil.innerHTML = data[i].name
+				texte.textContent = "Magnifique Appareil, il fera votre bonheur"
+				description.appendChild(nameAppareil);
+				description.appendChild (texte)
+
+			//Mise en place du prix au sein du figcaption
+				prix.setAttribute("class", "prix");
+				prix.innerHTML = data [i].price;
+				figcaption.appendChild(prix);
+
+			//Mise en place des boutons et liens au sein du figcaption
+				let boutons = document.createElement('div');
+				let liensBouton = document.createElement('a');
+				let bouton = document.createElement ("button")
+				boutons.setAttribute("class", "boutons");
+				figcaption.appendChild(boutons);
+				boutons.appendChild(liensBouton);
+				bouton.appendChild("button")
+				}
+			});
+}
+
+appareilPhoto ();
--- Frontend/js/panier.js
+++ Frontend/js/panier.js
@@ -6,36 +6,11 @@ let listeAppareilsPhoto = document.getElementById("liste-appareils-photo"); //r�
 let figure = document.createElement("figure"); //création d'une figure
 listeAppareilsPhoto.appendChild(figure); // figure est un enfant de listeAppareilsPhoto 
 
-//Création de l'img au sein de l'élément figure
-let imageAppareil= document.createElement('img');
-imageAppareil.setAttribute('src="../images/app-photo-1.jpg"');
-imageAppareil.setAttribute('alt', 'appareil photo vintage');
-figure.appendChild(imageAppareil);
 
-//Création du figcaption au sein de l'élément figure
-let figcaption= document.createElement('figcaption');
-figure.appendChild(figcaption);
 
-//Mise en place du texte au sein de la figcaption
-let description = document.createElement('div');
-let prix = document.createElement('div');
-description.setAttribute('class', 'description');
-prix.setAttribute('class', 'prix');
-figcaption.appendChild(description);
-figcaption.appendChild(prix);
 
-//Mise en place des boutons au sein de la figcaption
-let boutons = document.createElement('div');
-boutons.setAttribute('class', 'boutons');
-figcaption.appendChild(boutons);
 
-//Mise en place du texte au sein de la class description
-let h3 = document.createElement('h3');
-let p = document.createElement('p');
-description.appendChild (p)
-description.appendChild(h3);
 
-//Mise en place des button au sein de la class boutons
-let liensBouton = document.createElement('a');
-boutons.appendChild(liensBouton);
+
+
 
