05afdce8e3603702b341ea86ef7028a6168e3de9
Johan <irejo@hotmail.fr>
2021-06-01 12:30:50 +0200

    NaN

--- Frontend/js/panier.js
+++ Frontend/js/panier.js
@@ -6,7 +6,7 @@ let mainHTML = document.querySelector('main');
 if ("monPanier" in localStorage) {
     panier = JSON.parse(localStorage.getItem('monPanier'));
     affichageCommande();
-} else {
+} else { // si absence du panier retour vers la page index
     window.location.href =`index.html`;
 }
 
@@ -19,33 +19,36 @@ function affichageCommande() {
     for (let i = 0; i < panier.length; i++) {
         let img = panier[i].img;
         let name = panier[i].name;
-        let prix = panier[i].price;
+        let prix= panier[i].price;
         let quantity = panier[i].quantity;
-        
-        // destructuration
 
         // Affichage des données obtenues au sein du localStorage
-        let article =
-            `<article>
-                <div>
-                <h3>${name}</h3>
-                    <img class="article__img" src=${img} alt=${name}>
+        let table =
+            `<form id="form-panier">
+                <div id="img-texte">
+                    <img class="form-img" src=${img}>
+                    <h3>${name}</h3>
                 </div>
-                <div>
-                    <p>Quantité : <span class="articleQuantity">${quantity}</p>
-                    <p>Prix : <span class="prixArticle">${prix * quantity}</span>€</p>
+                <div id="quantite-prix">
+                    <p>Quantité : ${quantity}</p>
+                    <p>Prix : <span class="articlePrice">${prix*quantity}</span>€</p>
                 </div>
-                <div>
+                <div class="quantite quantite-panier">
+                    <div class="value-button" id="decrease" onclick="decreaseValue()" value="Decrease Value">-</div>
+                    <input name="number" type="number" id="number"  placeholder="0"  onchange="total(value)">
+                    <div class="value-button" id="increase" onclick="increaseValue()" value="Increase Value">+</div>
+                </div>
+                <div id="btn-panier">
                     <button class="btn">
                     <i class="fas fa-times"></i>
                     </button>
                 </div>
-            </article>`;
+            </form>`;
 
            
-        affichage.innerHTML += article;
+        affichage.innerHTML += table;
     }
-    // Insertion au HTML + appel fonction prix total
+    // Création du  HTML et affichage de la fonction  totalPrice
     mainHTML.appendChild(affichage);
     totalPrice();
 }
@@ -53,10 +56,10 @@ function affichageCommande() {
 // Calcul du prix total
 function totalPrice() {
     let affichagePrix = document.createElement('div');
-    affichagePrix.className = "affichage_prix";
-    let priceArray = document.querySelectorAll('.prixArticle');
+    affichagePrix.className = "affichage-prix";
+    let priceInfo = document.querySelectorAll('.prixArticle');
     let sum = 0;
-    priceArray.forEach(i => {
+    priceInfo.forEach(i => {
        i = Number(i.textContent);
        sum += i;
     });
@@ -64,7 +67,7 @@ function totalPrice() {
     mainHTML.appendChild(affichagePrix);
 }
 
-// Supprimer un article du panier
+// Fonction pour supprimer les articles du panier
 let suppr = document.querySelectorAll('.btn');
 for (let i = 0; i < suppr.length; i++) {
         suppr[i].addEventListener('click', () => {
@@ -74,19 +77,19 @@ for (let i = 0; i < suppr.length; i++) {
             // S'il n'y a plus rien, on vide le localStorage
             if (panier.length < 1) {
                 localStorage.removeItem('monPanier');
-            }
 
-            // Rafraîchissement de la page
-            window.location.reload();
+              // Rafraîchissement de la page
+              window.location.reload();
+            }
         })
 }
 
-// Création d'un event pour enregistrer les informations du client
+//  addEventListener mis en place pour obtenir les coordonnées du client
 document.querySelector('form').addEventListener('submit', (e) => {
-    // On bloque le comportement par défaut du navigateur
+    // Blocage du comportement par défaut 
     e.preventDefault();
 
-    // Création de l'objet qui récupèrera les informations du formulaire avec un tableau produits
+    // Création du formulaire qui récupèrera les données du client avec un tableau  nommé "produits"
     let formInfo = {
         contact : {
             prenom: document.getElementById('prenom').value,
@@ -99,32 +102,30 @@ document.querySelector('form').addEventListener('submit', (e) => {
     }
 
 
-    // Vérification sur le prénom
+    // Code mis en place pour que le client transmette les données correctement 
     if(!/^[ a-zA-ZàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅåÆæœ'`'\-]+$/.test(data.contact.prenom)) {
         alert(" Attention, écrivez correctement votre prénom!!");
         return
     }
 
-    // Vérification sur le Nom
     if(!/^[ a-zA-ZàèìòùÀÈÌÒÙáéíóúýÁÉÍÓÚÝâêîôûÂÊÎÔÛãñõÃÑÕäëïöüÿÄËÏÖÜŸçÇßØøÅåÆæœ'`'\-]+$/.test(data.contact.nom)) {
         alert("Attention, écrivez correctement votre nom!!");
         return
     }
 
-    // Vérification sur l'adresse (Ville) avec entre 5 et 20 lettres
     if(!/^[ a-zA-Zéèê\-]{5,20}/.test(data.contact.ville)) {
         alert("Attention, écrivez correctement le nom de votre ville (au minimum 5 lettres)");
         return
     }
 
-    // On fait rentrer dans data.products les id des articles autant de fois que la quantity est demandée
+    // fonction que l'on fait rentrer dans data.products pour s'adapter à la quantité demandée
     panier.forEach(elt => {
         for (let i = 0; i < elt.quantity; i++) {
             formInfo.produits.push(elt.id);    
         }
     })
 
-    // Envoi fetch via la méthode POST de 'data'
+    // Fetch créé avec la méthode POST 
     fetch('http://localhost:3000/api/cameras/order', {
         method: 'POST',
         headers: {
@@ -132,12 +133,12 @@ document.querySelector('form').addEventListener('submit', (e) => {
         },
         body: JSON.stringify(data),
     })
-    // Retour de l'envoi au server
-    .then(response => response.json())
+    // Retour de l'envoi au serveur
+    .then(res => res.json())
     .then(data => {
-        // On va mettre dans le localStorage 'data'
+        // Insertion au sein du localStorage 'data'
         localStorage.setItem('confirmation', JSON.stringify(data));
-        // Effaçons le panier vu que la commande est passée
+        // Effacement du panier lorsque la commande est passée
         localStorage.removeItem('monPanier');
         // Ouverture de la page de confirmation
         location.replace("confirmation.html")
@@ -147,3 +148,4 @@ document.querySelector('form').addEventListener('submit', (e) => {
         console.error(error);
     });
 })
+
--- Frontend/js/produit.js
+++ Frontend/js/produit.js
@@ -3,18 +3,19 @@ const getUrl = new URL(window.location.href);
 const id = getUrl.searchParams.get('id');
 const form = document.getElementById('selection');
 const commandes = document.getElementById('commandes');
-const prix = document.querySelector('.prix');
-const coutTotal = document.querySelector('.coutTotal');
 
 // Fonction qui récupère et affiche l'article
 function appareilChoix (id) {
     fetch('http://localhost:3000/api/cameras/' + id) // Article affiché par l'id en fonction de la sélection en page index
     .then((res) => res.json()) // Ici nous transmettons les données au format JSON
     .then((data) => {
+
          // Transmission des informations reçues au fichier HTML de façon dynamique
         document.querySelector('h3').innerHTML = data.name;
         document.querySelector('.image').src = data.imageUrl;
-        document.querySelector('.prix').innerHTML = (new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(data.price/1000));
+        let prix= data.price / 100;
+        document.querySelector('.prix').innerHTML = prix;
+        
         let optionsLentilles = data.lenses.length;
         for(let i = 0; i < optionsLentilles; i++){
           document.getElementById('options').innerHTML +=  `<option>${data.lenses[i]}</option>`;
@@ -23,13 +24,6 @@ function appareilChoix (id) {
 		);}
    
 appareilChoix (id);// Appel de la fonction, pour affichage des données
-
- // Fonction pour afficher le coût total
- function total(value) {
-  let result = quantity * prix.innerHTML;
-  coutTotal.innerHTML = result;
-}
-console.log(total);
 
   // variable panier
 let panier;
@@ -49,8 +43,8 @@ function ajoutPanier(e) {
         id,
         name : document.querySelector('h3').textContent,
         img : document.querySelector('.image').src,
-        quantity : document.querySelector('#number').value,
-        prix : document.querySelector('.prix').value,
+        quantity : Number (document.querySelector('#quantityNumber').value),
+        prix : Number (document.querySelector('.prix').textContent),
         options : document.querySelector('#options').value
     }
 
@@ -60,11 +54,17 @@ function ajoutPanier(e) {
         return;
     }
 
+    for (let i = 0; i < panier.length; i++) {
+        if (commande.options == panier[i].options && commande.id == panier[i].id) {
+            commande.quantity += Number(panier[i].quantity);
+            panier.splice(i,1);
+        }
+    }
 
     // Informations transmises au panier + reset des valeurs
     panier.push(commande);
     form.reset();
-    coutTotal.innerHTML = 0;
+   
     
     // Placement du panier dans le localStorage
     localStorage.setItem('monPanier', JSON.stringify(panier));
@@ -73,19 +73,7 @@ function ajoutPanier(e) {
 // Click qui permet de placer le panier dans le localStorage
 commandes.addEventListener('click', ajoutPanier);
 
-//Fonctions pour augmenter et baisser la quantité
-function increaseValue() {
-	var value = parseInt(document.getElementById('number').value, 10);
-	value = isNaN(value) ? 0 : value;
-	value++;
-	document.getElementById('number').value = value;
-  }
-  
-  function decreaseValue() {
-	var value = parseInt(document.getElementById('number').value, 10);
-	value = isNaN(value) ? 0 : value;
-	value < 1 ? value = 1 : '';
-	value--;
-	document.getElementById('number').value = value;
-  }
+
+ 
+
  
