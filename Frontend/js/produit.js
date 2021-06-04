9dbc5f825354403ebdb343eddc24c1aabfef59cf
Johan <irejo@hotmail.fr>
2021-06-04 15:14:41 +0200

    quantité

--- Frontend/js/produit.js
+++ Frontend/js/produit.js
@@ -1,76 +1,78 @@
-// Variables qui seront utiles pour la suite du développement de cette page
-const getUrl = new URL(window.location.href);
-const id = getUrl.searchParams.get('id');
-const form = document.getElementById('selection');
-const commandes = document.getElementById('commandes');
+// Variables pour récupérer id
+const getUrlId = window.location.search;
+const getUrlParams = new URLSearchParams(getUrlId);
+const id = getUrlParams.get("id");
 
+
+// Variables pour structurer le produit
+let structureProduit = document.querySelector('#main');
+let product = ''
 // Fonction qui récupère et affiche l'article
-function appareilChoix (id) {
-    fetch('http://localhost:3000/api/cameras/' + id) // Article affiché par l'id en fonction de la sélection en page index
+function appareilChoix (){
+    fetch(`http://localhost:3000/api/cameras/${id}`) // Article affiché par l'id en fonction de la sélection en page index
     .then((res) => res.json()) // Ici nous transmettons les données au format JSON
-    .then((data) => {
+    .then( appareil => {
+               
+         product =
+                `<figure id="produit">
+                <img class="image" src="${appareil.imageUrl}">
+                <figcaption id ="capture">
+                    <div class="description">
+                        <h3>${appareil.name}</h3>
+                        <p> Magnifique appareil photo il fera votre bonheur</p>
+                    </div>
+                    <p class="prix">${appareil.price/1000}€</p>
+                </figcaption>
+            </figure>
+        <form id="selection">
+            <div id="choix">  
+                <div id="nombre">         
+                    <p>Quantité:</p>
+                    <div class="quantite quantite-panier"> 
+                        <div class ="plus" type="button"><i class="fas fa-plus"></i></div>
+                        <input type="number" id="quantity" value="0">
+                        <div class ="moins" type="button"><i class="fas fa-minus"></i></div>
+                    </div>
+                </div> 
+            </div> `;
+            product +=  `
+            <div id="type"> 
+                <div id="valeur">
+                    <p ="options">Lentilles:</p>
+                    <select id ="options">
+                </div>
+            </div>  `
+            appareil.lenses.forEach(lentilles => {
+                product += 
+                `<option value= "${lentilles}">${lentilles}</option> `
+            });
+            product +=  ` </select>
+            <div id="boutons">
+                <button id="commandes">Ajoutez au panier</button>
+            </div>
+        </form>`
 
-         // Transmission des informations reçues au fichier HTML de façon dynamique
-        document.querySelector('h3').innerHTML = data.name;
-        document.querySelector('.image').src = data.imageUrl;
-        document.querySelector('.prix').textContent = data.price/1000;
-        let optionsLentilles = data.lenses.length;
-        for(let i = 0; i < optionsLentilles; i++){
-          document.getElementById('options').innerHTML +=  `<option>${data.lenses[i]}</option>`;
-        }
-      }	
-		);}
-   
-appareilChoix (id);// Appel de la fonction, pour affichage des données
+        const fragment = document.createRange()
+        .createContextualFragment(product)
+        document.getElementById('main').appendChild(fragment)
+         
+})
 
-  // Création variable panier
-let panier;
-
-if ("monPanier" in localStorage) {
-    panier = JSON.parse(localStorage.getItem('monPanier'));
-} else {
-    panier = [];
+.catch ((err) => {
+	alert (err);
+})
 }
-// Fonction qui récupère les éléments pour les transmettre à la page panier
-function ajoutPanier(e) {
-    //Blocage de  l'action par défaut du navigateur
-    e.preventDefault();
+  
+appareilChoix ();// Appel de la fonction, pour affichage des données
 
-    // Création des éléments qui seront envoyés dans le localStorage
-    let commande = {
-        id,
-        name : document.querySelector('h3').textContent,
-        img : document.querySelector('.image').src,
-        quantity : Number (document.querySelector('#quantityNumber').value),
-        prix : Number (document.querySelector('.prix').textContent),
-        options : document.querySelector('#options').value
-    }
+let qtyPlus = document.querySelector (".plus");
+const qtyMoins = document.querySelector (".moins");
+let quantity = document.querySelector("#quantity");
 
-    // Vérification que la commande possède au moins un article
-    if (!commande.quantity > 0) {
-        alert("vous devez choisir au moins un article pour passer la commande !")
-        return;
-    }
-
-    for (let i = 0; i < panier.length; i++) {
-        if (commande.options == panier[i].options && commande.id == panier[i].id) {
-            commande.quantity += Number(panier[i].quantity);
-            panier.splice(i,1);
-        }
-    }
-
-    // Informations transmises au panier + reset des valeurs
-    panier.push(commande);
-    form.reset();
-   
-    // Placement du panier dans le localStorage
-    localStorage.setItem('monPanier', JSON.stringify(panier));
-
-}
-// Click qui permet de placer le panier dans le localStorage
-commandes.addEventListener('click', ajoutPanier);
+qtyplus.addEventListener("click",(e) =>{
+    quantity.innerHTML = parseInt(quantity.innerHTML) + 1
+});
 
 
- 
 
  
